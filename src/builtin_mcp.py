093cdb10bf7c10278feed5ffbcb3a3d32c6549b4
LynxZizzle <84462396+LynxZizzle@users.noreply.github.com>
2026-07-01 13:41:51 +1000

    Add files via upload

--- src/builtin_mcp.py
+++ src/builtin_mcp.py
@@ -82,7 +82,7 @@ _BUILTIN_NPX_SERVERS = {
     "builtin_browser": {
         "name": "Built-in: Browser",
         "command": "npx",
-        "args": ["-y", "@playwright/mcp@latest", "--headless", "--caps", "vision"],
+        "args": ["npx.cmd", "-y", "@playwright/mcp@latest", "--config", "playwright-mcp-config.json"],,
     }
 }
 
